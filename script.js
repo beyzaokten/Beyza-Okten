d5fe180e566adff8bc7291f228114239b85f7915
Beyza Ökten <95380183+beyzaokten@users.noreply.github.com>
2026-04-16 23:42:07 +0300

    Click handler added

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ function calculateTorques() {
 function calculateAngle(leftTorque,rightTorque) {
     return Math.max(-30, Math.min(30, (rightTorque-leftTorque)/10));
 }
+
+//Click Handler
+const plank = document.getElementById('plank');
+
+plank.addEventListener('click',function(e){
+    //Tıklama pozisyonu 
+    const rect = plank.getBoundingClientRect();
+    const clickX = e.clientX - rect.left;
+
+    const plankWidth = plank.offsetWidth; //400px
+    const pivotX = plankWidth /2; //Merkez = 200px
+
+    const distance = Math.abs(clickX-pivotX);
+    const side = clickX < pivotX ? 'left' : 'right';
+
+    //Random ağırlık
+    const weight = Math.floor(Math.random()*10)+1;
+
+    const obj = {
+        id: Date.now(),
+        weight:weight,
+        distance:distance,
+        side:side,
+        clickX:clickX
+
+    };
+
+    objects.push(obj);
+    
+})
